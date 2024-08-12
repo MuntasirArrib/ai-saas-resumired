8115d96cdc177bf97a63c7a9fb49a8d3cb368d6e
MuntasirArrib <mantasirahmed@gmail.com>
2024-08-13 03:17:33 +1000

    landingpagefinall

--- components/landing-video.tsx
+++ components/landing-video.tsx
@@ -1,20 +1,24 @@
 "use client";
 
-import Video from 'next-video';
-import myVideo from '/videos/videodemo2.mp4'; 
 import { useAuth } from "@clerk/nextjs";
 
- 
 export const LandingVideo = () => {
-    const { isSignedIn } = useAuth();
-    return (
-        <div className="px-10 pb-20">
-            <div className="max-w-7xl mx-auto">
-                <Video 
-                    src={myVideo} 
-                    className="w-full rounded-2xl overflow-hidden" 
-                />
-            </div>
+  const { isSignedIn } = useAuth();
+
+  return (
+    <div className="px-10 pb-20">
+      <div className="max-w-7xl mx-auto">
+        <div className="relative pb-[56.25%] h-0 overflow-hidden rounded-2xl">
+          <iframe
+            src="https://www.youtube.com/embed/Af-pjFbl_ZQ"  // Replace with your YouTube video ID
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            className="absolute top-0 left-0 w-full h-full"
+          ></iframe>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
